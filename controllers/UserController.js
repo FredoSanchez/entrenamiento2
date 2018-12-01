3e1f2f822c3c1fe0540ba85645ed9133174cc2f4
Fredy Sanchez <00082817@uca.edu.sv>
2018-12-01 07:50:37 -0600

    Controller terminado

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,14 +3,102 @@ var userModel = require('../models/User');
 
 let controller = {};
 
-controller.getAll = function(req,res){};
 
-controller.getOne = function(req,res){};
+controller.getAll = function(req,res){
+    userModel.find({},function(err, users){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                users
+            });
+        }
+    });
+};
 
-controller.update = function(req,res){};
+controller.getOne = function(req,res){
+    userModel.findOne({_id: req.params.id}, function(err, user){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                user
+            });
+        }
+    });
+};
 
-controller.insert = function(req,res){};
+controller.update = function(req,res){
 
-controller.delete = function(req,res){};
+    let update = {
+        username: req.body.username,
+        name: req.body.name,
+        lastname: req.body.lastname
+    };
+    userModel.findByIdAndUpdate(req.params.id, update, function(err, old){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                old,
+                update
+            });
+        }
+    });
+};
+
+controller.insert = function(req,res){
+    let userNew = new userModel({
+        username: req.body.username,
+        name: req.body.name,
+        lastname: req.body.lastname
+    });
+    userNew.save(function(err, insertado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                insertado
+            });
+        }
+    });
+};
+
+controller.delete = function(req,res){
+    userModel.findByIdAndRemove(req.params.id, function(err, eliminado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                eliminado
+            });
+        }
+    });
+};
 
 module.exports = controller;
